8f276c8f98d1433dbe5f23205c16c5177c8c40be
opilane <opilane@HP-CZC0359NN9>
2023-02-21 09:21:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dictionary_ja_foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new Dictionary<int, string>
+            {
+                {1, "Tööstushariduskeskus" },
+                {2, "Tallinna Polütehnikum" },
+                {3, "Tallinna 21.Kool" },
+            };
+
+            result.ToList().ForEach(x => Console.WriteLine($"{x.Key} - {x.Value}"));
+        }
+    }
+}
